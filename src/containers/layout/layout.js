9096a2cdf08031de674187ff6aa5d048d6ca45e2
Marti Belegu <48526054+mgbelegu@users.noreply.github.com>
2020-11-25 16:52:19 +0100

    update

--- src/containers/layout/layout.js
+++ src/containers/layout/layout.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Aux from "../hoc/Auxiliary";
 import Navigation from "../navigation/navigation";
-import Footer from "../footer/footer";
 import "./layout.css";
 
 class Layout extends Component {
